3b35ca67ce4a336e4339124ee09d69bff66a1220
heloysouza <heloiltonsl@gmail.com>
2025-10-17 21:01:51 -0300

    alt. no db.js

--- config/db.js
+++ config/db.js
@@ -3,8 +3,7 @@ import mongoose from "mongoose";
 const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
+      
     });
     console.log("✅ MongoDB conectado com sucesso!");
   } catch (error) {
